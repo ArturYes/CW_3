fa9f560734f7c17399e69f42b9f5d8ead3e4d8a1
artur <youzen0you@gmail.com>
2024-01-22 19:52:06 +0600

    change

--- курсовая_3/main.py
+++
@@ -1,8 +0,0 @@
-import json
-from utils import display_last_5_transactions
-
-
-if __name__ == "__main__":
-    with open('operations.json', 'r') as file:
-        data = json.load(file)
-        display_last_5_transactions(data)
