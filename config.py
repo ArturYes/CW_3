27f2e2b2471ff776f40ece03c321f44d0cc2cf65
artur <youzen0you@gmail.com>
2024-01-23 23:36:29 +0600

    add path

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
 ROOT_DIR = os.path.dirname(__file__)
-TEST_JSON_PATH = os.path.join(ROOT_DIR, 'CW_3', 'test_json_data.json')
+JSON_PATH = os.path.join(ROOT_DIR, 'data', 'operations.json')
+TEST_JSON_PATH = os.path.join(ROOT_DIR, 'tests', 'test_json_data.json')
+TEST_2_JSON_PATH = os.path.join(ROOT_DIR, 'tests', 'test_load_json.json')
